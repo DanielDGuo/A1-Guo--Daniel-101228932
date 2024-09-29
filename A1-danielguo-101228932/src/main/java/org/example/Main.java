8b554a01d0647a633a8ef606b54b3cdae6d5a50a
Daniel Guo <onyxeagle274@gmail.com>
2024-09-29 19:19:34 -0400

    R-CODE-01
    Code to initialize shuffled decks

--- A1-danielguo-101228932/src/main/java/org/example/Main.java
+++ A1-danielguo-101228932/src/main/java/org/example/Main.java
@@ -2,48 +2,201 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public ArrayList<Card> AdDeck;
     public ArrayList<Card> EvDeck;
-    public final ArrayList<Card> AdventureDeckList = new ArrayList<Card>();
-    public final ArrayList<Card> EventDeckList = new ArrayList<Card>();
+    //list of all 100 cards included in the deck
+    public final ArrayList<Card> AdventureDeckList = new ArrayList<Card>(
+            Arrays.asList(
+                    new Card("F5", "Foe", 5),
+                    new Card("F5", "Foe", 5),
+                    new Card("F5", "Foe", 5),
+                    new Card("F5", "Foe", 5),
+                    new Card("F5", "Foe", 5),
+                    new Card("F5", "Foe", 5),
+                    new Card("F5", "Foe", 5),
+                    new Card("F5", "Foe", 5),
+                    new Card("F10", "Foe", 10),
+                    new Card("F10", "Foe", 10),
+                    new Card("F10", "Foe", 10),
+                    new Card("F10", "Foe", 10),
+                    new Card("F10", "Foe", 10),
+                    new Card("F10", "Foe", 10),
+                    new Card("F10", "Foe", 10),
+                    new Card("F15", "Foe", 15),
+                    new Card("F15", "Foe", 15),
+                    new Card("F15", "Foe", 15),
+                    new Card("F15", "Foe", 15),
+                    new Card("F15", "Foe", 15),
+                    new Card("F15", "Foe", 15),
+                    new Card("F15", "Foe", 15),
+                    new Card("F15", "Foe", 15),
+                    new Card("F20", "Foe", 20),
+                    new Card("F20", "Foe", 20),
+                    new Card("F20", "Foe", 20),
+                    new Card("F20", "Foe", 20),
+                    new Card("F20", "Foe", 20),
+                    new Card("F20", "Foe", 20),
+                    new Card("F20", "Foe", 20),
+                    new Card("F25", "Foe", 25),
+                    new Card("F25", "Foe", 25),
+                    new Card("F25", "Foe", 25),
+                    new Card("F25", "Foe", 25),
+                    new Card("F25", "Foe", 25),
+                    new Card("F25", "Foe", 25),
+                    new Card("F25", "Foe", 25),
+                    new Card("F30", "Foe", 30),
+                    new Card("F30", "Foe", 30),
+                    new Card("F30", "Foe", 30),
+                    new Card("F30", "Foe", 30),
+                    new Card("F35", "Foe", 35),
+                    new Card("F35", "Foe", 35),
+                    new Card("F35", "Foe", 35),
+                    new Card("F35", "Foe", 35),
+                    new Card("F40", "Foe", 40),
+                    new Card("F40", "Foe", 40),
+                    new Card("F50", "Foe", 50),
+                    new Card("F50", "Foe", 50),
+                    new Card("F70", "Foe", 70),
+                    new Card("D5", "Weapon", 5),
+                    new Card("D5", "Weapon", 5),
+                    new Card("D5", "Weapon", 5),
+                    new Card("D5", "Weapon", 5),
+                    new Card("D5", "Weapon", 5),
+                    new Card("D5", "Weapon", 5),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("H10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("S10", "Weapon", 10),
+                    new Card("B15", "Weapon", 15),
+                    new Card("B15", "Weapon", 15),
+                    new Card("B15", "Weapon", 15),
+                    new Card("B15", "Weapon", 15),
+                    new Card("B15", "Weapon", 15),
+                    new Card("B15", "Weapon", 15),
+                    new Card("B15", "Weapon", 15),
+                    new Card("B15", "Weapon", 15),
+                    new Card("L20", "Weapon", 20),
+                    new Card("L20", "Weapon", 20),
+                    new Card("L20", "Weapon", 20),
+                    new Card("L20", "Weapon", 20),
+                    new Card("L20", "Weapon", 20),
+                    new Card("L20", "Weapon", 20),
+                    new Card("E30", "Weapon", 30),
+                    new Card("E30", "Weapon", 30)
+            )
+    );
+    //list of all 17 cards included in the deck
+    public final ArrayList<Card> EventDeckList = new ArrayList<Card>(
+            Arrays.asList(
+                    new Card("Q2", "Quest", 2),
+                    new Card("Q2", "Quest", 2),
+                    new Card("Q2", "Quest", 2),
+                    new Card("Q3", "Quest", 3),
+                    new Card("Q3", "Quest", 3),
+                    new Card("Q3", "Quest", 3),
+                    new Card("Q3", "Quest", 3),
+                    new Card("Q4", "Quest", 4),
+                    new Card("Q4", "Quest", 4),
+                    new Card("Q4", "Quest", 4),
+                    new Card("Q5", "Quest", 5),
+                    new Card("Q5", "Quest", 5),
+                    new Card("Plague", "Event", 0),
+                    new Card("Queen's Favour", "Event", 0),
+                    new Card("Queen's Favour", "Event", 0),
+                    new Card("Prosperity", "Event", 0),
+                    new Card("Prosperity", "Event", 0)
+            )
+    );
 
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        Main game = new Main();
+        game.initializeAdventureDeck();
+        game.initializeEventDeck();
     }
 
     class Card {
-    }
+        //valid ID include but aren't limited to: F5, F10, D, H, S, Q2, Q3, Plague, Queen's favor
+        String id;
+        //Types include Foe, Weapon, Quest, and Event
+        String type;
+        //values are the number associated with the card; such as a 5 power foe, or a 30 power excalibur
+        int value;
 
+        //constructor
+        public Card(String id, String type, int value) {
+            this.id = id;
+            this.type = type;
+            this.value = value;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        @Override
+        public String toString() {
+            return id;
+        }
+    }
 
     public void initializeAdventureDeck() {
-    }
-
-    public ArrayList<Card> getAdventureDeck() {
-        return AdDeck;
+        AdDeck = new ArrayList<>(AdventureDeckList);
+        Collections.shuffle(AdDeck);
     }
 
     public int getAdventureDeckSize() {
-        return 0;
+        return AdDeck.size();
     }
 
     public ArrayList<Card> getAdventureDeckCards() {
-        return new ArrayList<Card>();
+        return AdDeck;
     }
 
     public void initializeEventDeck() {
-    }
-
-    public ArrayList<Card> getEventDeck() {
-        return EvDeck;
+        EvDeck = new ArrayList<>(EventDeckList);
+        Collections.shuffle(EvDeck);
     }
 
     public ArrayList<Card> getEventDeckCards() {
-        return new ArrayList<Card>();
+        return EvDeck;
     }
 
     public int getEventDeckSize() {
-        return 0;
+        return EvDeck.size();
     }
 }
